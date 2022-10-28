faa420680e939a7771a6ce67d7ee1825b3afb88d
Tolu Folorunso <tolufolorunso@yahoo.com>
2022-10-28 17:42:23 +0100

    made register and login page responsive

--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -17,9 +17,15 @@ const login = (req, res, next) => {
 };
 
 const register = (req, res, next) => {
-  console.log('hello');
   const payload = {
-    pageTitle: 'Welcome Home',
+    pageTitle: 'Register | Page',
+    error: '',
+    firstname: '',
+    lastname: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
   };
   res.status(200).render('auth/register', payload);
 };
